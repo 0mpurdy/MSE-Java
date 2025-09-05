0a9931c22bd37d8628f989b08580c38e1005f4ea
Michael Purdy <0mpurdy@gmail.com>
2025-09-05 20:57:13 +0100

    Fix cramped start up size

--- MSE/src/main/java/com/zerompurdy/mse/MSE.java
+++ MSE/src/main/java/com/zerompurdy/mse/MSE.java
@@ -11,7 +11,7 @@ public class MSE extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MSE.class.getResource("FXMLSearch.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 515, 340);
         stage.setTitle("MSE");
         stage.setScene(scene);
         stage.show();
